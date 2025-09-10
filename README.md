545b1bd3b0d2dccf30501b3868e758cba5d4ea5d
Nico Singh <4417761+nicosingh@users.noreply.github.com>
2025-09-10 20:35:27 -0300

    Refactor Terraform workflow: rename 'Terraform Plan' step to 'Terraform Apply' and add auto-fixes workflow for formatting and documentation updates

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clase-8-iac-duoc-sep-25
+
+<!-- BEGIN_TF_DOCS -->
+<!-- END_TF_DOCS -->
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "infraestructura_azure" {
   source = "git::https://github.com/nicosingh/terraform-azurerm-duoc-iac-sep-25.git?ref=03c55adb6449b915a92db38491a1878b5e58ff65"
 
   ambiente              = "desarrollo"
-  nombre_grupo_recursos = "AreaInfraestructura"
+  nombre_grupo_recursos  = "AreaInfraestructura"
   nombre_proyecto       = "nico-clase-8"
   ubicacion             = "eastus2"
 }
